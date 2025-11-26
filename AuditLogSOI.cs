4b5949d6a975290dd57736057ad4c7275cecc8b5
Jon Swoveland <jswoveland@users.noreply.github.com>
2025-11-25 21:12:08 -0800

    Add project files.

---
+++ AuditLogSOI.cs
@@ -0,0 +1,227 @@
+﻿// Copyright 2018 ESRI
+// 
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+// 
+// You may freely redistribute and use this sample code, with or
+// without modification, provided you include the original copyright
+// notice and use restrictions.
+// 
+// See the use restrictions at <your Enterprise SDK install location>/userestrictions.txt.
+// 
+
+using ESRI.ArcGIS.Carto;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geodatabase;
+using ESRI.ArcGIS.Geometry;
+using ESRI.ArcGIS.Server;
+using ESRI.Server.SOESupport;
+using ESRI.Server.SOESupport.SOI;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.Json;
+using EsriPBuffer;
+using Google.Protobuf;
+
+
+//This is REST SOE template of Enterprise SDK
+
+//TODO: sign the project (project properties > signing tab > sign the assembly)
+//      this is strongly suggested if the dll will be registered using regasm.exe <your>.dll /codebase
+
+
+namespace AuditLogSOI
+{
+    [ComVisible(true)]
+    [Guid("e61db380-0c77-4138-8064-50b0a656b902")]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ServerObjectInterceptor("MapServer",
+        Description = "",
+        DisplayName = "AuditLogSOI",
+        Properties = "",
+        SupportsSharedInstances = false)]
+    public class AuditLogSOI : IServerObjectExtension, IRESTRequestHandler, IWebRequestHandler, IRequestHandler2, IRequestHandler
+    {
+        private string _soiName;
+        private IServerObjectHelper _soHelper;
+        private ServerLogger _serverLog;
+        private RestSOIHelper _restSOIHelper;
+
+        public AuditLogSOI()
+        {
+            _soiName = this.GetType().Name;
+        }
+
+        public void Init(IServerObjectHelper pSOH)
+        {
+            _soHelper = pSOH;
+            _serverLog = new ServerLogger();
+            _restSOIHelper = new RestSOIHelper(pSOH);
+            _serverLog.LogMessage(ServerLogger.msgType.infoStandard, _soiName + ".init()", 200, "Initialized " + _soiName + " SOI.");
+        }
+
+        public void Shutdown()
+        {
+            _serverLog.LogMessage(ServerLogger.msgType.infoStandard, _soiName + ".init()", 200, "Shutting down " + _soiName + " SOI.");
+        }
+
+        #region REST interceptors
+
+        public string GetSchema()
+        {
+            IRESTRequestHandler restRequestHandler = _restSOIHelper.FindRequestHandlerDelegate<IRESTRequestHandler>();
+            if (restRequestHandler == null)
+                return null;
+
+            return restRequestHandler.GetSchema();
+        }
+
+        public byte[] HandleRESTRequest(string Capabilities, string resourceName, string operationName,
+            string operationInput, string outputFormat, string requestProperties, out string responseProperties)
+        {
+            responseProperties = null;
+            _serverLog.LogMessage(ServerLogger.msgType.infoStandard, _soiName + ".HandleRESTRequest()",
+                200, "Request received in Sample Object Interceptor for handleRESTRequest");
+
+            IRESTRequestHandler restRequestHandler = _restSOIHelper.FindRequestHandlerDelegate<IRESTRequestHandler>();
+            if (restRequestHandler == null)
+                return null;
+
+            /*
+            * Add code to manipulate REST requests here
+            */
+            if (operationName == "query")
+            {
+                var loginUser = "current user=" + SOIBase.GetServerEnvironment().UserInfo.Name;
+                string userAction = String.Format("SOI Intercepted Query. Capabilities={0}, resourceName={1}, operationName={2}, operationInput={3}, outputFormat={4}, " +
+                    "requestProperties={5}, loginUser={6}", Capabilities, resourceName, operationName, operationInput, outputFormat, requestProperties, loginUser);
+
+                _serverLog.LogMessage(ServerLogger.msgType.infoSimple, "HandleRESTRequest()", 200, userAction);
+
+                byte[] originalResponse = restRequestHandler.HandleRESTRequest(Capabilities, resourceName, operationName, operationInput, "pbf", requestProperties, out responseProperties);
+
+                string resultJSONStr = null;
+
+                if(outputFormat == "pbf") { 
+                    var featureCollection = FeatureCollectionPBuffer.Parser.ParseFrom(originalResponse);
+                    resultJSONStr = JsonFormatter.Default.Format(featureCollection);
+                    
+                }else if (outputFormat == "json")
+                {
+                    resultJSONStr = System.Text.Encoding.UTF8.GetString(originalResponse);
+                }
+                if(!String.IsNullOrEmpty(resultJSONStr))
+                {
+                    _serverLog.LogMessage(ServerLogger.msgType.infoSimple, "HandleRESTRequest()", 200, resultJSONStr);
+                     JsonObject resultJSON = new JsonObject(resultJSONStr);
+                }
+
+
+
+            }
+            else
+            {
+                string userAction = String.Format("SOI Intercepted {2}. Capabilities={0}, resourceName={1}, operationInput={3}, outputFormat={4}, " +
+                    "requestProperties={5}", Capabilities, resourceName, operationName, operationInput, outputFormat, requestProperties);
+
+                _serverLog.LogMessage(ServerLogger.msgType.infoSimple, "HandleRESTRequest()", 200, userAction);
+            }
+
+            return restRequestHandler.HandleRESTRequest(
+                    Capabilities, resourceName, operationName, operationInput,
+                    outputFormat, requestProperties, out responseProperties);
+        }
+
+        #endregion
+
+        #region SOAP interceptors
+
+        public byte[] HandleStringWebRequest(esriHttpMethod httpMethod, string requestURL,
+            string queryString, string Capabilities, string requestData,
+            out string responseContentType, out esriWebResponseDataType respDataType)
+        {
+            _serverLog.LogMessage(ServerLogger.msgType.infoStandard, _soiName + ".HandleStringWebRequest()",
+                200, "Request received in Sample Object Interceptor for HandleStringWebRequest");
+
+            /*
+             * Add code to manipulate requests here
+             */
+
+            IWebRequestHandler webRequestHandler = _restSOIHelper.FindRequestHandlerDelegate<IWebRequestHandler>();
+            if (webRequestHandler != null)
+            {
+                return webRequestHandler.HandleStringWebRequest(
+                        httpMethod, requestURL, queryString, Capabilities, requestData, out responseContentType, out respDataType);
+            }
+
+            responseContentType = null;
+            respDataType = esriWebResponseDataType.esriWRDTPayload;
+            //Insert error response here.
+            return null;
+        }
+
+        public byte[] HandleBinaryRequest(ref byte[] request)
+        {
+            _serverLog.LogMessage(ServerLogger.msgType.infoStandard, _soiName + ".HandleBinaryRequest()",
+                  200, "Request received in Sample Object Interceptor for HandleBinaryRequest");
+
+            /*
+             * Add code to manipulate requests here
+             */
+
+            IRequestHandler requestHandler = _restSOIHelper.FindRequestHandlerDelegate<IRequestHandler>();
+            if (requestHandler != null)
+            {
+                return requestHandler.HandleBinaryRequest(request);
+            }
+
+            //Insert error response here.
+            return null;
+        }
+
+        public byte[] HandleBinaryRequest2(string Capabilities, ref byte[] request)
+        {
+            _serverLog.LogMessage(ServerLogger.msgType.infoStandard, _soiName + ".HandleBinaryRequest2()",
+                  200, "Request received in Sample Object Interceptor for HandleBinaryRequest2");
+
+            /*
+             * Add code to manipulate requests here
+             */
+
+            IRequestHandler2 requestHandler = _restSOIHelper.FindRequestHandlerDelegate<IRequestHandler2>();
+            if (requestHandler != null)
+            {
+                return requestHandler.HandleBinaryRequest2(Capabilities, request);
+            }
+
+            //Insert error response here.
+            return null;
+        }
+
+        public string HandleStringRequest(string Capabilities, string request)
+        {
+            _serverLog.LogMessage(ServerLogger.msgType.infoStandard, _soiName + ".HandleStringRequest()",
+                   200, "Request received in Sample Object Interceptor for HandleStringRequest");
+
+            /*
+             * Add code to manipulate requests here
+             */
+
+            IRequestHandler requestHandler = _restSOIHelper.FindRequestHandlerDelegate<IRequestHandler>();
+            if (requestHandler != null)
+            {
+                return requestHandler.HandleStringRequest(Capabilities, request);
+            }
+
+            //Insert error response here.
+            return null;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AuditLogSOI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AuditLogSOI")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("8c5ea9ff-2d77-4275-82bc-765ab39e9b61")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: ESRI.Server.SOESupport.AddInPackage("AuditLogSOI", "ea582d73-7192-458c-a58e-7420969ee5cb",
+    Author = "jswoveland",
+    Company = "",
+    Date = "11/24/2025 10:42:12 AM",
+    Description = "",
+    TargetProduct = "Server",
+    TargetVersion = "11.4",
+    Version = "1.0")]
